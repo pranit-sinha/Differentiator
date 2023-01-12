c8b366a9f4680747b9f5f564f3506915454a384e
Aakash Rao <mrao.aakash@gmail.com>
2023-01-12 13:51:00 +0530

    Corrected class error

--- EfficientNetB0.py
+++ EfficientNetB0.py
@@ -60,7 +60,7 @@ model.add(layers.GlobalMaxPooling2D(name="gap"))
 model.add(layers.Flatten(name="flatten"))
 model.add(layers.Dropout(0.2, name="dropout_out"))
 model.add(layers.Dense(256, activation='relu', name="fc1"))
-model.add(layers.Dense(['CellSpace','WhiteSpace'], activation='softmax', name="fc_out"))
+model.add(layers.Dense(2, activation='softmax', name="fc_out"))
 
 if not os.path.exists(f'/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/EffnetDifferentiator/models'):
         os.makedirs(f'/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/EffnetDifferentiator/models')
