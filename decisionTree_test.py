be53d09c61e94ce7525f0aaa4069e26565ec897e
Aakash Rao <mrao.aakash@gmail.com>
2023-01-13 12:48:23 +0530

    Added final tester

--- decisionTree_test.py
+++ decisionTree_test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-data = pd.read_csv('master.csv')
+data = pd.read_csv('tester.csv')
 data = data.sample(frac=1)
 X = data[['R_mean', 'R_std', 'R_sum','G_mean', 'G_std', 'G_sum', 'B_mean', 'B_std', 'B_sum', 'total_mean', 'total_std', 'total_sum']]
 # Number of rows in X
@@ -17,23 +17,16 @@ X_test = X[int(X_rows*0.8):]
 y = data['class']
 y_train = y[:int(X_rows*0.8)]
 y_test = y[int(X_rows*0.8):]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X_train, y_train)
-
-# Predictions
+clf = joblib.load('finalized_model.sav')
 predictions = clf.predict(X)
-
-# Accuracy
 from sklearn.metrics import accuracy_score
 print(accuracy_score(y, predictions))
-filename = 'finalized_model.sav'
-joblib.dump(clf, filename)
 conf_mat = confusion_matrix(y, predictions)
 print(conf_mat)
 conf_mat = pd.DataFrame(conf_mat, columns=['whiteSpace', 'cellSpace'], index=['whiteSpace', 'cellSpace'])
-conf_mat.to_csv('confusion_matrix.csv')
+conf_mat.to_csv('tester_confusion_matrix.csv')
 sns.heatmap(conf_mat, annot=True, fmt='d')
-plt.savefig('confusion_matrix.png')
+plt.savefig('tester_confusion_matrix.png')
 
 # Classification Report
 cfr = classification_report(y, predictions)
